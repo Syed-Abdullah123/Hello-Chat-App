8730aad7924c717aa34c62e922c5c3f6b281b152
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-09 15:54:46 +0500

    contains dummy firebase config data

---
+++ Hello/src/firebase/config.ts
@@ -0,0 +1,23 @@
+// firebase.ts
+import { initializeApp } from 'firebase/app';
+import {initializeAuth, getReactNativePersistence} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  // Your Firebase config object from Firebase Console
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
+
+const FIREBASE_APP = initializeApp(firebaseConfig);
+
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
