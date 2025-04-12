d54ac92b89252db02d866fde6413a70f3fbb1a12
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-12 16:57:01 +0500

    called multiple custom components to display them here

--- Hello/src/screens/ChatScreen.tsx
+++ Hello/src/screens/ChatScreen.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { ChattingScreenHeaderComponent } from "../components/ChatScreenHeader";
+import SenderMessageComponent from "../components/SenderMsgComponent";
+import ReceiverMessageComponent from "../components/RecieverMsgComponent";
+import ChatInputComponent from "../components/ChatInputComponent";
 
-const ChatScreen = ({ route, navigation }) => {
-  useEffect(() => {
-    navigation.setOptions({
-      title: route.params.user.name,
-    });
-  });
+const ChatScreen = ({ navigation, route }: any) => {
+  const { user } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text>ChatScreen</Text>
+      <ChattingScreenHeaderComponent navigation={navigation} route={route} />
+      <SenderMessageComponent />
+      <ReceiverMessageComponent />
+      <SenderMessageComponent />
+      <ReceiverMessageComponent />
+      <SenderMessageComponent />
+      <ReceiverMessageComponent />
+      <SenderMessageComponent />
+
+      <ChatInputComponent />
+      {/* <FlatList
+        data={[]}
+        renderItem={() => (
+          <View>
+          </View>
+        )}
+      /> */}
     </View>
   );
 };
@@ -20,7 +36,8 @@ export default ChatScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#E5F5E4",
+    paddingTop: 25,
+    paddingHorizontal: 10,
   },
 });
