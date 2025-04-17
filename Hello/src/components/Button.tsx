b9d11020436cd11ca66dde6fec3d958d9560ffac
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-17 12:21:03 +0500

    custom reusable button component with shadow

--- Hello/src/components/Button.tsx
+++ Hello/src/components/Button.tsx
@@ -61,6 +61,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     alignItems: "center",
     justifyContent: "center",
+    elevation: 2,
   },
   buttonDisabled: {
     opacity: 0.6,
