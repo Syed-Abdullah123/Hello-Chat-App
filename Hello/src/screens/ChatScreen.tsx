29c565525a2de0366824a3de38e8b45d7e8645f2
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-18 12:18:30 +0500

    displaying messages in real-time during chat

--- Hello/src/screens/ChatScreen.tsx
+++ Hello/src/screens/ChatScreen.tsx
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { ChattingScreenHeaderComponent } from "../components/ChatScreenHeader";
+import socket from "../utils/socket";
+
 import SenderMessageComponent from "../components/SenderMsgComponent";
 import ReceiverMessageComponent from "../components/RecieverMsgComponent";
 import ChatInputComponent from "../components/ChatInputComponent";
 
+import { ChattingScreenHeaderComponent } from "../components/ChatScreenHeader";
+
+interface Message {
+  sender: string;
+  image: string;
+  message: string;
+}
+
 const ChatScreen = ({ navigation, route }: any) => {
   const { user } = route.params;
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessages((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    const msgData = {
+      sender: user.name,
+      image: user.image,
+      message,
+    };
+    socket.emit("send_message", msgData);
+    setMessages((prev) => [...prev, msgData]); // show immediately
+  };
 
   return (
     <View style={styles.container}>
       <ChattingScreenHeaderComponent navigation={navigation} route={route} />
-      <SenderMessageComponent />
-      <ReceiverMessageComponent item={user} />
-      <SenderMessageComponent />
-      <ReceiverMessageComponent item={user} />
-      <SenderMessageComponent />
-      <ReceiverMessageComponent item={user} />
+
+      <View>
+        <FlatList
+          data={messages}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({ item }) =>
+            item.sender === user.name ? (
+              <SenderMessageComponent item={item} />
+            ) : (
+              <ReceiverMessageComponent item={item} />
+            )
+          }
+        />
+      </View>
       <SenderMessageComponent />
 
-      <ChatInputComponent />
+      <ChatInputComponent onSend={sendMessage} />
     </View>
   );
 };
