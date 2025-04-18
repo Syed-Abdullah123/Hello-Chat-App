0cae93ada95c18b543a846eece98ef608ae77262
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-18 19:41:28 +0500

    backend server for the real-time chat using node and socket

--- hello-chat-server/index.ts
+++ hello-chat-server/index.ts
@@ -29,6 +29,7 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server is running on port ${PORT}`);
 });
