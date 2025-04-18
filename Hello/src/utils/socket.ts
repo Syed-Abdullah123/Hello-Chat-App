2a5879d4a16fabffbedce08e77821cc17690f71a
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-18 19:47:29 +0500

    connecting socket with the app using websocket transport

--- Hello/src/utils/socket.ts
+++ Hello/src/utils/socket.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://192.168.10.7:3000");
+const socket = io("http://192.168.10.7:3000", {
+    transports: ["websocket"],
+    forceNew: false,
+    reconnection: true
+});
 
 export default socket;
