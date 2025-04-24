2b6e1e4d82fdcb190ecd8042466ff6a9b4ef4e6e
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-24 21:45:54 +0500

    connecting socket using websocket and defined func for calling

--- Hello/src/utils/socket.ts
+++ Hello/src/utils/socket.ts
@@ -7,3 +7,53 @@ const socket = io("http://192.168.10.7:3000", {
 });
 
 export default socket;
+
+// Store current user ID for reference
+let currentUserId = null;
+
+export const connectSocket = (userId) => {
+  currentUserId = userId;
+  if (!socket.connected) {
+    socket.connect();
+    
+    // Register user ID with server
+    socket.emit('register_user', { userId });
+    
+    console.log('Socket connecting with user ID:', userId);
+  }
+};
+
+export const disconnectSocket = () => {
+  socket.disconnect();
+  currentUserId = null;
+};
+
+export const initiateCall = (receiverId, callType, caller) => {
+  console.log('Initiating call to:', receiverId, 'type:', callType);
+  socket.emit('initiate_call', {
+    to: receiverId,
+    callType,
+    caller
+  });
+};
+
+export const acceptCall = (callerId) => {
+  console.log('Accepting call from:', callerId);
+  socket.emit('accept_call', {
+    to: callerId
+  });
+};
+
+export const rejectCall = (callerId) => {
+  console.log('Rejecting call from:', callerId);
+  socket.emit('reject_call', {
+    to: callerId
+  });
+};
+
+export const endCall = (receiverId) => {
+  console.log('Ending call with:', receiverId);
+  socket.emit('end_call', {
+    to: receiverId
+  });
+};
