922896158920d8d5d84516ab6d03ec724f71db9a
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-04-25 12:13:48 +0500

    connecting socket when user is authenticated upon app start

--- Hello/App.tsx
+++ Hello/App.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import { FIREBASE_AUTH } from "./src/firebase/config";
+import { connectSocket } from "./src/utils/socket";
+
 import StackNavigator from "./src/navigation/StackNavigator";
 
 export default function App() {
+  useEffect(() => {
+    const unsubscribe = FIREBASE_AUTH.onAuthStateChanged((user) => {
+      if (user) {
+        // Connect socket when user is authenticated
+        connectSocket(user.uid);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return <StackNavigator />;
 }
